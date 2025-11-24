27125d590b7b2776435aa7e6a030f7072af1e77b
JudithV <judithvc2209@gmail.com>
2025-11-24 11:52:04 +0100

    add diagram generation script for MVINE architecture

---
+++ minkunext_vine/diagram_MVINE.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('../')
+from pycore.tikzeng import *
+
+# defined your arch
+arch = [
+    to_head( '..' ),
+    to_cor(),
+    to_begin(),
+    to_Conv("conv1", 512, 64, offset="(0,0,0)", to="(0,0,0)", height=64, depth=64, width=2 ),
+    to_Pool("bn1", offset="(0,0,0)", to="(conv1-east)"),
+    to_Pool("relu1", offset="(0,0,0)", to="(bn1-east)"),
+    to_Conv("conv2", 64, 64, offset="(1,0,0)", to="(relu1-east)", height=32, depth=32, width=2 ),
+    to_connection( "relu1", "conv2"),
+    to_Conv("conv2", 64, 64, offset="(0,0,0)", to="(0,0,0)", height=64, depth=64, width=2 ),
+    to_Pool("bn2", offset="(0,0,0)", to="(conv2-east)"),
+    to_Pool("relu2", offset="(0,0,0)", to="(bn2-east)"),
+    to_Conv("conv3", 64, 64, offset="(1,0,0)", to="(relu2-east)", height=32, depth=32, width=2 ),
+    to_connection( "relu2", "conv3"),
+    to_Pool("pool1", offset="(0,0,0)", to="(conv3-east)", height=28, depth=28, width=1),
+    #to_SoftMax("soft1", 10 ,"(3,0,0)", "(pool1-east)"),
+    #to_connection("conv3", "pool1"),
+    to_end()
+    ]
+
+def main():
+    namefile = str(sys.argv[0]).split('.')[0]
+    to_generate(arch, namefile + '.tex' )
+
+if __name__ == '__main__':
+    main()
